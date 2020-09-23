da6cb8ebaf7bfbca3b691385a36a9c0fafe89167
unknown <russeliihernandez@gmail.com>
2020-09-24 01:11:27 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP_CRUD_Tutorial</title>
+    <title>PHP_CRUD_Tutorial_GIT</title>
 </head>
     <style>
         html, body{
